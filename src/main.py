9d09db0c3ea4a376504ddd3e5436a9271195ef99
Alfonso Cannavale <alfycan@gmail.com>
2024-07-02 20:18:02 +0200

    feat: add export of results to main

--- src/main.py
+++ src/main.py
@@ -3,10 +3,31 @@ import pandas as pd
 from data.loader import load_dataset
 from analysis.symptom_calculator import SymptomCalculator
 from mutation.mutation_operator import MutationOperator
+from datetime import datetime
 
 # Function to check if a string contains a single word
 def is_single_word(text):
     return len(text.split()) == 1
+
+def store_results(column, column_type, mutation_type, is_sensitive, pre_symptoms, post_symptoms):
+    pre_symptoms_prefixed = {f'pre_{key}': value for key, value in pre_symptoms.items()}
+    post_symptoms_prefixed = {f'post_{key}': value for key, value in post_symptoms.items()}
+
+    results.append({
+        'column': column,
+        'column_type': column_type, # 'text' or 'numeric
+        'mutation_type': mutation_type,
+        'is_sensitive': is_sensitive,
+        **pre_symptoms_prefixed,
+        **post_symptoms_prefixed
+    })
+
+def export_results_to_csv():
+    df_results = pd.DataFrame(results)
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    filename = f'../results/results_{timestamp}.csv'
+    df_results.to_csv(filename, index=False)
+    print(f'Results exported successfully to {filename}')
 
 if __name__ == '__main__':
     
@@ -35,6 +56,8 @@ if __name__ == '__main__':
     if target_attribute not in df.columns:
         raise ValueError(f"The target attribute {target_attribute} does not exist in the dataset.")
         exit(1)
+
+    results = []
 
     # Calculate the symptoms for each sensitive attribute
     for sensitive_attribute in sensitive_attributes:
@@ -86,48 +109,54 @@ if __name__ == '__main__':
             if col != target_attribute:
                 if pd.api.types.is_numeric_dtype(df[col]):
                     # Apply a numeric mutation operator randomly
-                    chosen_operator = np.random.choice(['increment_decrement_feature', 'swap_values', 'scale_values', 'discrete_binning'])
+                    operators = ['increment_decrement_feature', 'swap_values', 'scale_values', 'discrete_binning']
                 else:
                     # Check if all rows in the column contain a single word
                     if all(is_single_word(text) for text in df[col]):
                         # Exclude augment_text if all rows have a single word
-                        chosen_operator = np.random.choice(['replace_synonyms', 'add_noise', 'random_category_assignment', 'swap_values'])
+                        operators = ['replace_synonyms', 'add_noise', 'random_category_assignment', 'swap_values']
                     else:
                         # Include augment_text among the available text-based operators and exclude random_category_assignment
-                        chosen_operator = np.random.choice(['augment_text', 'replace_synonyms', 'add_noise', 'swap_values'])
+                        operators = ['augment_text', 'replace_synonyms', 'add_noise', 'swap_values']
             
-                print(f"\n\n\nApplying {chosen_operator} to column {col}.")
-                # Call the chosen mutation operator method dynamically
-                if chosen_operator == 'increment_decrement_feature':
-                    df_new = mutation_operator.increment_decrement_feature(col, increment=np.random.choice([True, False]), amount=np.random.uniform(1, 10), percentage=30)
-                elif chosen_operator == 'swap_values':
-                    df_new = mutation_operator.swap_values(col, num_swaps=int(df.shape[0] * 0.1))
-                elif chosen_operator == 'scale_values':
-                    df_new = mutation_operator.scale_values(col, scale_factor=np.random.uniform(0.5, 1.5), percentage=30)
-                elif chosen_operator == 'discrete_binning':
-                    bins = np.linspace(df[col].min(), df[col].max(), num=4) # Split the range into 4 bins
-                    df_new = mutation_operator.discrete_binning(col, bins=bins)
-                elif chosen_operator == 'random_category_assignment':
-                    df_new = mutation_operator.random_category_assignment(col, percentage=30)
-                elif chosen_operator == 'augment_text':
-                    df_new = mutation_operator.augment_text(col, percentage=20)
-                elif chosen_operator == 'replace_synonyms':
-                    df_new = mutation_operator.replace_synonyms(col, row_percentage=20, word_percentage=20)
-                elif chosen_operator == 'add_noise':
-                    df_new = mutation_operator.add_noise(col, noise_chance=0.1)
-                print("Mutation applied successfully.")
+                for operator in operators:
+                    print(f"\n\n\nApplying {operator} to column {col}.")
+                    # Call the mutation operator method dynamically
+                    if operator == 'increment_decrement_feature':
+                        df_new = mutation_operator.increment_decrement_feature(col, increment=np.random.choice([True, False]), amount=np.random.uniform(1, 10), percentage=30)
+                    elif operator == 'swap_values':
+                        df_new = mutation_operator.swap_values(col, num_swaps=int(df.shape[0] * 0.1))
+                    elif operator == 'scale_values':
+                        df_new = mutation_operator.scale_values(col, scale_factor=np.random.uniform(0.5, 1.5), percentage=30)
+                    elif operator == 'discrete_binning':
+                        bins = np.linspace(df[col].min(), df[col].max(), num=4) # Split the range into 4 bins
+                        df_new = mutation_operator.discrete_binning(col, bins=bins)
+                    elif operator == 'random_category_assignment':
+                        df_new = mutation_operator.random_category_assignment(col, percentage=30)
+                    elif operator == 'augment_text':
+                        df_new = mutation_operator.augment_text(col, percentage=20)
+                    elif operator == 'replace_synonyms':
+                        df_new = mutation_operator.replace_synonyms(col, row_percentage=20, word_percentage=20)
+                    elif operator == 'add_noise':
+                        df_new = mutation_operator.add_noise(col, noise_chance=0.1)
+                    print("Mutation applied successfully.")
 
-                # Recalculating symptoms after mutation
-                symptom_calculator = SymptomCalculator(df_new, sensitive_attribute, target_attribute)
-                new_symptoms = symptom_calculator.calculate_symptoms(privileged, unprivileged)
+                    # Recalculating symptoms after mutation
+                    symptom_calculator = SymptomCalculator(df_new, sensitive_attribute, target_attribute)
+                    new_symptoms = symptom_calculator.calculate_symptoms(privileged, unprivileged)
 
-                # Compare pre and post mutation symptoms
-                print(f"\nComparing symptoms for {col}:")
-                for symptom, value in new_symptoms.items():
-                    old_value = symptoms.get(symptom, None)
-                    if old_value:
-                        change = value - old_value
-                        if change != 0:
-                            print(f"{symptom} changed by {change}")
-                        else:
-                            print(f"{symptom} remained the same")
+                    column_type = 'numeric' if pd.api.types.is_numeric_dtype(df[col]) else 'text'
+                    store_results(col, column_type, operator, col in sensitive_attributes, symptoms, new_symptoms)
+
+                    # Compare pre and post mutation symptoms
+                    print(f"\nComparing symptoms for {col}:")
+                    for symptom, value in new_symptoms.items():
+                        old_value = symptoms.get(symptom, None)
+                        if old_value:
+                            change = value - old_value
+                            if change != 0:
+                                print(f"{symptom} changed by {change}")
+                            else:
+                                print(f"{symptom} remained the same")
+            
+    export_results_to_csv()      
